9fb5a6b27f842fcb8a935b1f1d3c31ff413f1586
Simran Kaur <simran143663@gmail.com>
2023-04-06 13:01:48 -0500

    Updated the seed file to add data from csv file to products and categories table

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,19 +11,65 @@ Product.destroy_all
 AdminUser.destroy_all
 Category.destroy_all
 
-filename = Rails.root.join("db/products.csv")
+filename = Rails.root.join("db/product.csv")
 puts "Loading products from the csv file: #{filename}"
 
-csv_data = File.read(filename)
-data = CSV.parse(csv_data, headers: true, encoding: "iso-8859-1")
+csv_data_product = File.read(filename)
+data = CSV.parse(csv_data_product, headers: true, encoding: "iso-8859-1")
 
-data.each do |p|
-  Product.create(
-    name:       p["name"],
-    price:      p["price"],
-    image:      p["image"]
-  )
+data.each do |m|
+  category = Category.find_or_create_by(name: m["category"])
+  if category && category.valid?
+    product = category.products.create(
+      name:       m["product"],
+      brand:      m["brand"],
+      price:      m["price"]
+    )
+
+    puts "Invalid product #{m['product']}" unless product&.valid?
+
+  else
+    puts "invalid category #{m['category']} for product #{m['product']}."
+  end
+
+  # puts m['original_title']
 end
-puts "Created #{Product.count} products"
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# file = Rails.root.join("db/categories.csv")
+# puts "Loading products from the csv file: #{file}"
+
+# csv_data = File.read(file)
+# categories = CSV.parse(csv_data, headers: true, encoding: "iso-8859-1")
+
+# categories.each do |c|
+#   Category.create(
+#     name: c["name"]
+#   )
+# end
+# puts "Created #{Category.count} category records"
+
+
+
+# filename = Rails.root.join("db/products.csv")
+# puts "Loading products from the csv file: #{filename}"
+
+# csv_data_product = File.read(filename)
+# products = CSV.parse(csv_data_product, headers: true, encoding: "iso-8859-1")
+
+# data.each do |p|
+#   Product.create(
+#     name:       p["product"],
+#     category_id:   p["category"],
+#     brand:      p["brand"],
+#     price:      p["price"],
+#     image:      p["image"]
+
+
+#   )
+# end
+# puts "Created #{Product.count} products"
+
+if Rails.env.development?
+  AdminUser.create!(email: 'admin@example.com', password: 'password',
+                    password_confirmation: 'password')
+end
