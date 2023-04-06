73e28a7b46c9a0cf2330ca7afb6250424192d720
Simran Kaur <simran143663@gmail.com>
2023-04-06 10:46:02 -0500

    Added brand column in products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "csv"
+
+Product.destroy_all
+AdminUser.destroy_all
+Category.destroy_all
+
+filename = Rails.root.join("db/products.csv")
+puts "Loading products from the csv file: #{filename}"
+
+csv_data = File.read(filename)
+data = CSV.parse(csv_data, headers: true, encoding: "iso-8859-1")
+
+data.each do |p|
+  Product.create(
+    name:       p["name"],
+    price:      p["price"],
+    image:      p["image"]
+  )
+end
+puts "Created #{Product.count} products"
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
