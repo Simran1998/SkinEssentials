f7ec0d4037b8bf5b265fd1890233814f717848f6
Simran Kaur <simran143663@gmail.com>
2023-04-10 13:32:00 -0500

    Impletmented location based breadcrumb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,9 @@ class ProductsController < ApplicationController
   end
 
   def show
+
     @product = Product.find(params[:id])
+    @category = @product.category
   end
 
   def search
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+  def breadcrumbs(controller, action)
+    crumbs = []
+    if controller == "products" && action == "index"
+      crumbs << link_to("Home", root_path)
+    elsif controller == "products" && action == "show"
+      crumbs << link_to("Home", root_path)
+      crumbs << link_to("Products", products_path)
+      crumbs << link_to(@product.category.name, category_path(@product.category))
+      crumbs << @product.name
+    elsif controller == "categories" && action == "show"
+      crumbs << link_to("Home", root_path)
+      crumbs << link_to("Categories", categories_path)
+      crumbs << @category.name
+    end
+    crumb_str = ""
+    crumbs.each_with_index do |crumb, i|
+      if i == crumbs.length - 1
+        crumb_str += "<span class='active'>#{crumb}</span>"
+      else
+        crumb_str += "#{crumb} &raquo; "
+      end
+    end
+    crumb_str.html_safe
+  end
 end
