851275689fd9015d14b8e7f32ce93ed40eec26a4
Simran Kaur <simran143663@gmail.com>
2023-04-12 14:52:33 -0500

    Added the functionality to add and item to the cart along with displaying a flash method

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,18 @@
 class CartController < ApplicationController
   def create
+    logger.debug("Adding #{params[:id]} to cart.")
+    id = params[:id].to_i
+    #session[:shopping_cart] << id # pushes id onto the end of the array
+    product = Product.find(id)
 
+    if session[:shopping_cart].include?(id) # pushes id onto the end of the array
+      flash[:notice] = "✚ #{product.name} is already in the cart."
+    elsif session[:shopping_cart] << id
+      flash[:notice] = "✚ #{product.name} added to cart."
+    end
+
+    redirect_to cart_index_path
   end
+
+
 end
